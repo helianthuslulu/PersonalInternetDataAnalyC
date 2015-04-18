20ec05957585d22bba2d3de532f6e73bea22888d
xiaofateng <xiaofateng@gmail.com>
2015-04-19 00:30:16 +0800

    newtest

---
+++ PersonalInternetDataAnaly/src/main/java/test/NewTest.java
@@ -0,0 +1,17 @@
+package test;
+/**   
+ * @Title: NewTest.java 
+ * @Package test 
+ * @Description: TODO(用一句话描述该文件做什么) 
+ * @author 肖发腾 xiaofateng@gmail.com   
+ * @date 2015年4月19日 上午12:29:29 
+ * @version V1.0   
+ */
+public class NewTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
